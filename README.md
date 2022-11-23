0bb4002295f0b123b639d98570c3af03a54d0458
mesaquejunior <mesaquejunior@gmail.com>
2022-11-23 19:28:52 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This project aims to build a simple server with Node JS without any libraries.
---
+++ app.js
@@ -0,0 +1,7 @@
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
+const http = require("http")
+const routes = require("./routes")
+
+const server = http.createServer(routes.handler)
+console.log(routes.message)
+server.listen(3000)
