af052fb94f9e77fc6b2f1780ee3d52e32b5fac4c
mesaquejunior <mesaquejunior@gmail.com>
2022-11-23 21:08:28 +0000

    sharing content between modules

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const path = require("path")
 
-const adminRouter = require("./routes/admin")
+const adminData = require("./routes/admin")
 const shopRouter = require("./routes/shop")
 
 const rootDir = require("./utils/path")
@@ -12,7 +12,7 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(rootDir, "..", "public")))
 
-app.use("/admin", adminRouter)
+app.use("/admin", adminData.router)
 app.use(shopRouter)
 
 app.use((req, res, next) => {
