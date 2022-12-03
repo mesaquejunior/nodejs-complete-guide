846593aaa34dd2385fa8a7d60f4f6b4fc47a8e79
mesaquejunior <mesaquejunior@gmail.com>
2022-12-03 19:36:51 +0000

    working with template engine ejs

--- src/app.js
+++ src/app.js
@@ -9,12 +9,7 @@ const { engine } = require('express-handlebars');
 
 const app = express()
 
-app.engine("hbs", engine({
-  layoutsDir: 'src/views/layouts/',
-  defaultLayout: 'main-layout',
-  extname: 'hbs'
-}))
-app.set("view engine", "hbs")
+app.set("view engine", "ejs")
 app.set("views", path.join(rootDir, "views"))
 
 const adminData = require("./routes/admin")
