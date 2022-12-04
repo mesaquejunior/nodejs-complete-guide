a594077a4974fb0fdd84094fb2dadb22e26c65bf
mesaquejunior <mesaquejunior@gmail.com>
2022-12-04 12:39:44 +0000

    fixing app issues

--- src/app.js
+++ src/app.js
@@ -2,10 +2,8 @@ const path = require("path")
 
 const rootDir = require("./utils/path")
 
-
 const express = require("express")
 const bodyParser = require("body-parser")
-const { engine } = require('express-handlebars');
 
 const app = express()
 
@@ -22,7 +20,7 @@ app.use("/admin", adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "I'm sorry! :´(" })
+  res.status(404).render("404", { pageTitle: "I'm sorry! :´(", path: "" })
 })
 
 app.listen(3000)
